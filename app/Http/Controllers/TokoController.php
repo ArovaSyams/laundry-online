180c5a43b927e391e62e3d70bd43319d7a13bb79
ArovaSyams <wifqoarova17@gmail.com>
2021-10-14 23:02:30 +0700

    add validation dan pesan error di halaman update data toko

--- app/Http/Controllers/TokoController.php
+++ app/Http/Controllers/TokoController.php
@@ -93,24 +93,44 @@ class TokoController extends Controller
      */
     public function update(Request $request, $id)
     {
-        
-
-        Toko::find($id)->update([
-            'nama_toko' => $request->nama_toko,
-            'nama_pemilik' => $request->nama_pemilik,
-            'alamat' => $request->alamat,
-            'provinsi' => $request->provinsi,
-            'kota' => $request->kota,
-            'kecamatan' => $request->kecamatan,
-            'kelurahan' => $request->kelurahan,
-            'foto_1' => $request->foto_1,
-            'foto_2' => $request->foto_2,
-            'foto_3' => $request->foto_3,
-            'metode_penjualan' => $request->metode_penjualan,
-            'harga' => $request->harga,
-            'hari_kerja' => $request->hari_kerja,
-            'jam_buka' => $request->jam_buka,
+        $validatedData = $request->validate([
+            'nama_toko' => 'required|max:255',
+            'nama_pemilik' => 'required|max:255',
+            'alamat' => 'required',
+            'provinsi' => 'required|max:255',
+            'kota' => 'required|max:255',
+            'kecamatan' => 'required|max:255',
+            'kelurahan' => 'required|max:255',
+            'foto_1' => 'required|max:255',
+            'foto_2' => 'required|max:255',
+            'foto_3' => 'required|max:255',
+            'deskripsi' => 'required',
+            'metode_penjualan' => 'required',
+            'harga' => 'required|max:255',
+            'hari_kerja_mulai' => 'required',
+            'hari_kerja_sampai' => 'required',
+            'jam_buka_mulai' => 'required',
+            'jam_buka_sampai' => 'required'
         ]);
+
+        Toko::find($id)->update($validatedData);
+
+        // Toko::find($id)->update([
+        //     'nama_toko' => $request->nama_toko,
+        //     'nama_pemilik' => $request->nama_pemilik,
+        //     'alamat' => $request->alamat,
+        //     'provinsi' => $request->provinsi,
+        //     'kota' => $request->kota,
+        //     'kecamatan' => $request->kecamatan,
+        //     'kelurahan' => $request->kelurahan,
+        //     'foto_1' => $request->foto_1,
+        //     'foto_2' => $request->foto_2,
+        //     'foto_3' => $request->foto_3,
+        //     'metode_penjualan' => $request->metode_penjualan,
+        //     'harga' => $request->harga,
+        //     'hari_kerja' => $request->hari_kerja,
+        //     'jam_buka' => $request->jam_buka,
+        // ]);
 
         return redirect('toko');
     }
--- resources/views/toko/update.blade.php
+++ resources/views/toko/update.blade.php
@@ -23,43 +23,93 @@
                             @csrf
                             <div class="mb-3">
                                 <label for="nama_toko" class="form-label">Nama Toko</label>
-                                <input type="text" class="form-control" id="nama_toko" name="nama_toko" placeholder="{{ $toko->nama_toko }}" value="{{ $toko->nama_toko }}">
+                                <input type="text" class="form-control @error('nama_toko') is-invalid @enderror" id="nama_toko" name="nama_toko" placeholder="{{ $toko->nama_toko }}" value="{{ $toko->nama_toko }}">
+                                @error('nama_toko')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
                             </div>
                             <div class="mb-3">
                                 <label for="nama_pemilik" class="form-label">nama_pemilik</label>
-                                <input type="text" class="form-control" id="nama_pemilik" name="nama_pemilik" placeholder="{{ $toko->nama_pemilik }}" value="{{ $toko->nama_pemilik }}">
+                                <input type="text" class="form-control @error('nama_pemilik') is-invalid @enderror" id="nama_pemilik" name="nama_pemilik" placeholder="{{ $toko->nama_pemilik }}" value="{{ $toko->nama_pemilik }}">
+                                @error('nama_pemilik')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
                             </div>
                             <div class="mb-3">
                                 <label for="alamat" class="form-label">alamat</label>
-                                <input type="text" class="form-control" id="alamat" name="alamat" placeholder="{{ $toko->alamat }}" value="{{ $toko->alamat }}">
+                                <input type="text" class="form-control @error('alamat') is-invalid @enderror" id="alamat" name="alamat" placeholder="{{ $toko->alamat }}" value="{{ $toko->alamat }}">
+                                @error('alamat')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
                             </div>
                             <div class="mb-3">
                                 <label for="provinsi" class="form-label">provinsi</label>
-                                <input type="text" class="form-control" id="provinsi" name="provinsi" placeholder="{{ $toko->provinsi }}" value="{{ $toko->provinsi }}">
+                                <input type="text" class="form-control @error('provinsi') is-invalid @enderror" id="provinsi" name="provinsi" placeholder="{{ $toko->provinsi }}" value="{{ $toko->provinsi }}">
+                                @error('provinsi')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
                             </div>
                             <div class="mb-3">
                                 <label for="kota" class="form-label">kota</label>
-                                <input type="text" class="form-control" id="kota" name="kota" placeholder="{{ $toko->kota }}" value="{{ $toko->kota }}">
+                                <input type="text" class="form-control @error('kota') is-invalid @enderror" id="kota" name="kota" placeholder="{{ $toko->kota }}" value="{{ $toko->kota }}">
+                                @error('kota')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
                             </div>
                             <div class="mb-3">
                                 <label for="kecamatan" class="form-label">kecamatan</label>
-                                <input type="text" class="form-control" id="kecamatan" name="kecamatan" placeholder="{{ $toko->kecamatan }}" value="{{ $toko->kecamatan }}">
+                                <input type="text" class="form-control @error('kecamatan') is-invalid @enderror" id="kecamatan" name="kecamatan" placeholder="{{ $toko->kecamatan }}" value="{{ $toko->kecamatan }}">
+                                @error('kecamatan')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
                             </div>
                             <div class="mb-3">
                                 <label for="kelurahan" class="form-label">kelurahan</label>
-                                <input type="text" class="form-control" id="kelurahan" name="kelurahan" placeholder="{{ $toko->kelurahan }}" value="{{ $toko->kelurahan }}">
+                                <input type="text" class="form-control @error('kelurahan') is-invalid @enderror" id="kelurahan" name="kelurahan" placeholder="{{ $toko->kelurahan }}" value="{{ $toko->kelurahan }}">
+                                @error('kelurahan')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
                             </div>
                             <div class="mb-3">
                                 <label for="foto_1" class="form-label">foto_1</label>
-                                <input type="text" class="form-control" id="foto_1" name="foto_1" placeholder="{{ $toko->foto_1 }}" value="{{ $toko->foto_1 }}">
+                                <input type="text" class="form-control @error('foto_1') is-invalid @enderror" id="foto_1" name="foto_1" placeholder="{{ $toko->foto_1 }}" value="{{ $toko->foto_1 }}">
+                                @error('foto_1')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
                             </div>
                             <div class="mb-3">
                                 <label for="foto_2" class="form-label">foto_2</label>
-                                <input type="text" class="form-control" id="foto_2" name="foto_2" placeholder="{{ $toko->foto_2 }}" value="{{ $toko->foto_2 }}">
+                                <input type="text" class="form-control @error('foto_2') is-invalid @enderror" id="foto_2" name="foto_2" placeholder="{{ $toko->foto_2 }}" value="{{ $toko->foto_2 }}">
+                                @error('foto_2')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
                             </div>
                             <div class="mb-3">
                                 <label for="foto_3" class="form-label">foto_3</label>
-                                <input type="text" class="form-control" id="foto_3" name="foto_3" placeholder="{{ $toko->foto_3 }}" value="{{ $toko->foto_3 }}">
+                                <input type="text" class="form-control @error('foto_3') is-invalid @enderror" id="foto_3" name="foto_3" placeholder="{{ $toko->foto_3 }}" value="{{ $toko->foto_3 }}">
+                                @error('foto_3')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
                             </div>
                             <div class="mb-3">
                                 <label for="deskripsi" class="form-label">deskripsi</label>
@@ -87,7 +137,12 @@
 
                             <div class="mb-3">
                                 <label for="harga" class="form-label">harga</label>
-                                <input type="text" class="form-control" id="harga" name="harga" placeholder="{{ $toko->harga }}" value="{{ $toko->harga }}">
+                                <input type="text" class="form-control @error('harga') is-invalid @enderror" id="harga" name="harga" placeholder="{{ $toko->harga }}" value="{{ $toko->harga }}">
+                                @error('harga')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                             </div>
                             <label for="hari_kerja_mulai" class="form-label">hari_kerja</label>
                             <select class="form-select mb-3 @error('hari_kerja_mulai') is-invalid @enderror" id="hari_kerja_mulai" name="hari_kerja_mulai">
@@ -122,7 +177,7 @@
                             @enderror
 
                             <div class="mb-3">
-                                <label for="jam_buka_mulai" class="form-label">jam_tutup</label>
+                                <label for="jam_buka_mulai" class="form-label">jam_buka_mulai</label>
                                 <input type="text" class="form-control @error('jam_buka_mulai') is-invalid @enderror" id="jam_buka_mulai" name="jam_buka_mulai" placeholder="07:00 WIB" value="{{ $toko->jam_buka_mulai }}">
                                 @error('jam_buka_mulai')
                                     <div class="invalid-feedback">
@@ -131,7 +186,7 @@
                                 @enderror
                             </div>
                             <div class="mb-3">
-                                <label for="jam_buka_sampai" class="form-label">jam_buka_sampai</label>
+                                <label for="jam_buka_sampai" class="form-label">jam_tutup</label>
                                 <input type="text" class="form-control @error('jam_buka_sampai') is-invalid @enderror" id="jam_buka_sampai" name="jam_buka_sampai" placeholder="18:00 WIB" value="{{ $toko->jam_buka_sampai }}">
                                 @error('jam_buka_sampai')
                                     <div class="invalid-feedback">
